d7c96d30cf2746ca33e3cf26302dbce4463ff7f7
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-12 19:11:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 def assign_rooms(names)
   names.collect {|name| "Hello, #{name}! You'll be assigned to room #{names.index(name) + 1}!"}
 end
-def printer(attendees)
+def printer(names)
   names.each {|name| puts badge_maker(name)}
   assign_rooms(names).each {|message| puts message}
 end
