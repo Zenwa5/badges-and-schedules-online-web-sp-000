0ed86009b053a92dcf8ffc4b7dc413a2a2b4b56f
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-12 17:48:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,6 @@ def badge_maker(name)
     return "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  array = ["Hello, my name is #{badge}"]
-  attendees.each do |badge|
-    "Hello, my name is #{badge}"
+  attendees.each do |badge = "Hello, my name is #{badge}"|
+
 end
