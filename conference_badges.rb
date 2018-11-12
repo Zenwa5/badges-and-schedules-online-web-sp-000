9170db1fdc7aaec4352215b9baa44237456fef8d
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-12 18:52:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def assign_rooms(attendees)
   room = 1
   until room > 7
     attendees.collect do |name|
-    return "Hello, #{name}! You'll be assigned room #{room}!"
+    return "Hello, #{name}! You'll be assigned room #{attendees.index(name)}!"
     room +=1
     end
   end
