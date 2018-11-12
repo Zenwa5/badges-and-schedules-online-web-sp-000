38a4aa43d424adcf6f86eeeacb9f267a78e51dce
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-12 17:50:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ def badge_maker(name)
     return "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  attendees.each do |badge = "Hello, my name is #{badge}"|
+  attendees.each do |badge|
 end
 end
